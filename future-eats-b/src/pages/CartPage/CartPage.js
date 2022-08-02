3ae5fae1e05eecb6cbea77b3042a3cb8b5187585
Márleo Piber da Rosa <marleopr@hotmail.com>
2022-08-02 10:06:01 -0300

    Corpo do carrinho

--- future-eats-b/src/pages/CartPage/CartPage.js
+++ future-eats-b/src/pages/CartPage/CartPage.js
@@ -1,13 +1,65 @@
 import React from "react";
 import * as s from './styled-CartPage';
+import img_home from "./../../assets/img/home.png";
+import img_cart from "./../../assets/img/cart.png";
+import img_perfil from "./../../assets/img/perfil.png";
 
 export default function CartPage() {
 
   return (
     <s.General>
-      <s.Container>
-        <h1>Página de carrinho</h1>
-      </s.Container>
+      <s.Grid>
+        <s.Line1>
+          <h3>Meu carrinho</h3>
+        </s.Line1>
+        <s.Line2>
+          <div>
+            Endereço de entrega
+          </div>
+          <div>
+            Rua Alessandra Vieira, 42
+          </div>
+        </s.Line2>
+
+
+        <s.Line3>
+          Nome do restaurante
+        </s.Line3>
+
+        <s.Line4>
+          Lista de produtos do carrinho
+        </s.Line4>
+
+        <s.Line5>
+          <s.Ship>Frete: R$3,99</s.Ship>
+          <s.Total>
+            <div>Subtotal</div>
+            <div>R$9,99</div>
+          </s.Total>
+          <div>Forma de pagamento</div>
+          <hr></hr>
+          <s.Payment>
+            <s.Money>
+              <input type="checkbox"></input>
+              <label>Dinheiro</label>
+            </s.Money>
+            <s.Credit>
+              <input type="checkbox"></input>
+              <label>Cartão de Crédito</label>
+            </s.Credit>
+          </s.Payment>
+        </s.Line5>
+
+        <s.Line6>
+          <button>Confirmar</button>
+        </s.Line6>
+
+        <s.Line7>
+          <s.ImgFooter src={img_home} alt="Home" />
+          <s.ImgFooter src={img_cart} alt="Home" />
+          <s.ImgFooter src={img_perfil} alt="Home" />
+        </s.Line7>
+      </s.Grid>
     </s.General>
   )
 }
--- future-eats-b/src/pages/CartPage/styled-CartPage.js
+++ future-eats-b/src/pages/CartPage/styled-CartPage.js
@@ -10,13 +10,91 @@ export const General = styled.div`
   box-sizing: border-box;
 `
 
-export const Container = styled.div`
+export const Grid = styled.div`
+  width: 375px;
+  height: 667px;
+  display: grid;
+  border-radius: 20px;
+  grid-template-rows: 60px 40px 80px 1fr 150px 40px 60px;
+  /* min-width: 100vw; */
+  /* min-width: 100%; */
+  /* min-height: 100vh; */
+  box-sizing: border-box;
+  box-shadow: 0 5px 5px 0 rgba(0,0,0,0.2), 0 10px 10px 0 rgba(0,0,0,0.15);
+`
+
+export const Line1 = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  width: 375px;
-  height: 667px;
-  border-radius: 20px;
-  background-color: lightcyan;
-  box-shadow: 0 5px 5px 0 rgba(0,0,0,0.2), 0 10px 10px 0 rgba(0,0,0,0.15);
+  background-color: lightblue;
+`
+
+export const Line2 = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: lightgreen;
+`
+
+export const Search = styled.input`
+`
+
+export const Line3 = styled.div`
+  display: flex;
+  align-items: center;
+  overflow-x: auto;
+  background-color: lightcoral;
+`
+
+export const Category = styled.div`
+  margin: 0 10px;
+`
+
+export const Line4 = styled.div`
+  background-color: lightpink;
+
+`
+
+export const Line5 = styled.div`
+  background-color: aquamarine;
+`
+export const Ship = styled.div`
+  display: flex;
+  justify-content: end;
+`
+export const Total = styled.div`
+  display: flex;
+  justify-content: space-between;
+`
+export const Payment = styled.div`
+  display: flex;
+  flex-direction: column;
+
+`
+export const Money = styled.div`
+  /* display: flex;
+  flex-direction: column; */
+
+`
+export const Credit = styled.div`
+  /* display: flex;
+  flex-direction: column; */
+
+`
+export const Line6 = styled.div`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  background-color: #34dcdc;
+`
+export const Line7 = styled.div`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  /* background-color: lightcyan; */
+`
+
+export const ImgFooter = styled.img`
+  width: 30px
 `
