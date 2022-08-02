40ae04df7e6879673ac0d3e9145ac5ee6462014f
Leonardo José Couto Silva Lopes de Souza <leonardojcsl@gmail.com>
2022-08-02 10:59:44 -0300

    Criação página Perfil

--- future-eats-b/src/pages/ProfilePage/styled-ProfilePage.js
+++ future-eats-b/src/pages/ProfilePage/styled-ProfilePage.js
@@ -20,3 +20,94 @@ export const Container = styled.div`
   background-color: lightpink;
   box-shadow: 0 5px 5px 0 rgba(0,0,0,0.2), 0 10px 10px 0 rgba(0,0,0,0.15);
 `
+export const Grid = styled.div`
+  width: 375px;
+  height: 667px;
+  display: grid;
+  border-radius: 20px;
+  grid-template-rows: 60px 40px 80px 1fr 60px;
+  /* min-width: 100vw; */
+  /* min-width: 100%; */
+  /* min-height: 100vh; */
+  box-sizing: border-box;
+  box-shadow: 0 5px 5px 0 rgba(0,0,0,0.2), 0 10px 10px 0 rgba(0,0,0,0.15);
+`
+
+export const Line1 = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: lightblue;
+`
+
+export const Line2 = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: lightgreen;
+`
+
+export const Search = styled.input`
+`
+
+export const Line3 = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  overflow-x: auto;
+  background-color: lightcoral;
+`
+
+export const Category = styled.div`
+  margin: 0 10px;
+`
+
+export const Line4 = styled.div`
+  background-color: lightpink;
+
+`
+export const Ship = styled.div`
+  display: flex;
+  justify-content: end;
+`
+export const Total = styled.div`
+  display: flex;
+  justify-content: space-between;
+`
+export const Payment = styled.div`
+  display: flex;
+  flex-direction: column;
+
+`
+export const Money = styled.div`
+  /* display: flex;
+  flex-direction: column; */
+
+`
+export const Credit = styled.div`
+  /* display: flex;
+  flex-direction: column; */
+
+`
+
+export const Line5 = styled.div`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  /* background-color: lightcyan; */
+`
+
+export const ImgFooter = styled.img`
+  width: 30px
+`
+
+export const Left = styled.div`
+
+`
+export const Right = styled.div`
+
+`
+
+export const Img_edit = styled.img`
+  width: 20px;
+`
